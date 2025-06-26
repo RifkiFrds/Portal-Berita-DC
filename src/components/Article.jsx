7c12aeb0767c238342089077f78b8272a135d3e3
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-06-26 17:59:50 +0700

    article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,5 +1,12 @@
-import react from 'react'
+import React from 'react'
 
-function Article({ newArticle }) {
-    
+function Article({ title, content }) {
+  return (
+    <article className="p-4">
+      <h2 className="text-2xl font-bold mb-4">{title}</h2>
+      <p className="text-gray-700">{content}</p>
+    </article>
+  )
 }
+
+export default Article
