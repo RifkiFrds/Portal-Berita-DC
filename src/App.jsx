d057d13c8f4fd5d0db67ad8a3f5a8aa7f079f531
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-06-27 20:24:11 +0700

    add article

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Jumbotron from "./components/Jumbotron";
+import Article from "./components/Article";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   return (
     <>
     <Header siteTitle="Aplikasi React Pertamaku" /> 
-    <Jembrotan title="Hallo Portal Berita" subtitle="Selamat datang di portal berita kami" buttonText="Baca Selengkapnya"/>
+    <Jumbotron title="Hallo Portal Berita" subtitle="Selamat datang di portal berita kami" buttonText="Baca Selengkapnya"/>
 
     <main className="container my-4">
       <h2>Selamat Datang di Aplikasi React Pertamaku</h2>
       <p>Ini adalah aplikasi React pertama saya. Saya belajar banyak tentang komponen, props, dan state.</p>
       <p>Semoga aplikasi ini bermanfaat bagi saya dan orang lain yang ingin belajar React!</p>
     </main>
+
+    <Article title="Telah terjadi kerusakan jalan di daerah Jumudi Kota Tangerang" content="Kelurahan Jurumudi merupakan kelurahn yang telah hadi sejak tahun 1989, banyak kerusakan jalana yang di akibatkan oleh Pembangunan jalan Tol Benda, dengan ini masyrakat sekitar kesulitan untuk mengakses jalan setiap harinya"/>
+
     <Footer authorName="Muhamad Rifki Firdaus"/>
     </>
   );
