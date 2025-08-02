1fd4ae731497e4d25ee3df6951a58f774ceaa3ea
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-08-02 15:48:46 +0700

    update notFoundPage

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
   return (
-    <div className='relative text-blue-400 justify-center'>
+    <div className='relative text-blue-400 text-center'>
       <h1 className='text-center text-xl'>404</h1>
       <p>Maaf Kamu tersesat!</p>
+      <Link className='relative bg-gray-488 pt-23' to='/'>Back To Home</Link>
     </div>
   )
 }
